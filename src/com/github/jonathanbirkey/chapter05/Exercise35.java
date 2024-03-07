5f2cabe20cca677219cb0de1be7ae6cb079e39ad
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-07 11:35:52 -0500

    finished exercise 35

--- src/com/github/jonathanbirkey/chapter05/Exercise35.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise35.java
@@ -2,14 +2,17 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Summation) Write a program to compute the following summation:
+ *     <p>(1/1+sqrt(2)) + (1/sqrt(2)+sqrt(3)) + (1/sqrt(3)+sqrt(4)) + ... + (1/sqrt(624)+sqrt(625))
  */
 package com.github.jonathanbirkey.chapter05;
 
 public class Exercise35 {
   public static void main(String[] args) {
-    // TODO: solve
+    double summation = 0;
+    for (int i = 1; i <= 624; i++) {
+      summation += 1 / (Math.sqrt(i) + Math.sqrt(i + 1));
+    }
+    System.out.printf("Summation = %f", summation);
   }
 }

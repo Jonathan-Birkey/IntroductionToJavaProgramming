14340d21d71ba5f0f3af4f5b4d1b47607db405b8
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-07 14:57:50 -0500

    finished exercise 36

--- src/com/github/jonathanbirkey/chapter03/Exercise09.java
+++ src/com/github/jonathanbirkey/chapter03/Exercise09.java
@@ -10,8 +10,10 @@
  *     program that prompts the user to enter the first 9 digits and displays the 10-digit ISBN
  *     (including leading zeros). Your program should read the input as an integer. Here are sample
  *     runs:
- *     <p>Enter the first 9 digits of an ISBN as integer: 013601267 The ISBN-10 number is 0136012671
- *     Enter the first 9 digits of an ISBN as integer: 013031997 The ISBN-10 number is 013031997X
+ *     <p>Enter the first 9 digits of an ISBN as integer: 013601267
+ *     <p>The ISBN-10 number is 0136012671
+ *     <p>Enter the first 9 digits of an ISBN as integer: 013031997
+ *     <p>The ISBN-10 number is 013031997X
  */
 package com.github.jonathanbirkey.chapter03;
 
@@ -45,9 +47,9 @@ public class Exercise09 {
     int checksum =
         (d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 + d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9) % 11;
     if (checksum == 10) {
-      System.out.printf("%d%d%d%d%d%d%d%d%dX\n", d1, d2, d3, d4, d5, d6, d7, d8, d9);
+      System.out.printf("The ISBN-10 number is %d%d%d%d%d%d%d%d%dX\n", d1, d2, d3, d4, d5, d6, d7, d8, d9);
     } else {
-      System.out.printf("%d%d%d%d%d%d%d%d%d%d\n", d1, d2, d3, d4, d5, d6, d7, d8, d9, checksum);
+      System.out.printf("The ISBN-10 number is %d%d%d%d%d%d%d%d%d%d\n", d1, d2, d3, d4, d5, d6, d7, d8, d9, checksum);
     }
   }
 }
--- src/com/github/jonathanbirkey/chapter05/Exercise36.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise36.java
@@ -2,14 +2,47 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Business application: checking ISBN) Use loops to simplify Programming Exercise 3.9.
  */
 package com.github.jonathanbirkey.chapter05;
 
+import java.util.Scanner;
+
 public class Exercise36 {
   public static void main(String[] args) {
-    // TODO: solve
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the first 9 digits of an ISBN as integer: ");
+    int number = input.nextInt();
+    input.close();
+    
+    int tempNumber = number;
+    int leadingZeroCounter = 0;
+    int checksum = 0;
+
+    for (int i = 9; i > 0; i--) {
+    	int digit = tempNumber % 10;
+    	tempNumber = tempNumber / 10;
+    	if(digit == 0) {
+    		leadingZeroCounter++;
+    	}else {
+    		leadingZeroCounter = 0;
+    	}
+    	checksum += digit * i;
+    }
+    
+    checksum = checksum % 11;
+    System.out.printf("The ISBN-10 number is ");
+    
+    if (checksum == 10) {
+    	for(int j = 0; j < leadingZeroCounter; j++) {
+    		System.out.print("0");
+    	}
+    	System.out.printf("%dX", number);
+    } else {
+    	for(int j = 0; j < leadingZeroCounter; j++) {
+    		System.out.print("0");
+    	}
+    	System.out.printf("%d%d", number, checksum);
+    }
   }
 }

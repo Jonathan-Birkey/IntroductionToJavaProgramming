06ab66c0b20059faff3316d418e8626ea3cf9c3c
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-10 19:48:49 -0400

    finished exercise 37, this one was interesting to solve using only loops

--- src/com/github/jonathanbirkey/chapter05/Exercise37.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise37.java
@@ -2,14 +2,41 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Decimal to binary) Write a program that prompts the user to enter a decimal integer then
+ *     displays its corresponding binary value. Don’t use Java’s Integer.toBinaryString(int) in this
+ *     program.
  */
 package com.github.jonathanbirkey.chapter05;
 
+import java.util.Scanner;
+
 public class Exercise37 {
   public static void main(String[] args) {
-    // TODO: solve
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a decimal integer: ");
+    int decimal = input.nextInt();
+    input.close();
+
+    int count = 0;
+    int binary = 0;
+
+    while (decimal > 0) {
+      if (decimal % 2 == 0) {
+        binary += 0;
+      } else {
+        binary += 1;
+      }
+      decimal = decimal / 2;
+      if (decimal > 0) {
+        binary = binary * 10;
+      }
+
+      count++;
+    }
+
+    for (int i = 0; i < count; i++) {
+      System.out.printf("%d", binary % 10);
+      binary = binary / 10;
+    }
   }
 }

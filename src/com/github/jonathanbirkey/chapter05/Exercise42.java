54643ac0147c12082c6d13aa1a8ff17c2307c1d4
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-11 10:39:37 -0400

    finished exercise 42

--- src/com/github/jonathanbirkey/chapter05/Exercise42.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise42.java
@@ -2,14 +2,34 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Financial application: find the sales amount) Rewrite Programming Exercise 5.39 as
+ *     follows:
+ *     <p>Use a for loop instead of a do-while loop.
+ *     <p>Let the user enter COMMISSION_SOUGHT instead of fixing it as a constant.
  */
 package com.github.jonathanbirkey.chapter05;
 
+import java.util.Scanner;
+
 public class Exercise42 {
   public static void main(String[] args) {
-    // TODO: solve
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter commission sought: ");
+    double desiredCommission = input.nextDouble();
+    double commission = 0;
+    double sales = 0;
+
+    for (int i = 0; commission < desiredCommission; i++) {
+      if (sales <= 5000) {
+        commission = sales * 0.08;
+      } else if (sales <= 10000) {
+        commission = (5000 * 0.08) + ((sales - 5000) * 0.10);
+      } else {
+        commission = (5000 * 0.08) + (5000 * .10) + ((sales - 10000) * 0.12);
+      }
+      sales = i;
+    }
+
+    System.out.printf("You would need to make $%.2f in sales to make at least $%.2f in commision.", sales, desiredCommission);
   }
 }

c5756fd02a158bb484f7b243ca8f27d297c05081
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-10 20:33:43 -0400

    finished exercise 40

--- src/com/github/jonathanbirkey/chapter05/Exercise40.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise40.java
@@ -2,14 +2,25 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Simulation: heads or tails) Write a program that simulates flipping a coin one million
+ *     times and displays the number of heads and tails.
  */
 package com.github.jonathanbirkey.chapter05;
 
 public class Exercise40 {
   public static void main(String[] args) {
-    // TODO: solve
+    int coin = 0;
+    int heads = 0;
+    int tails = 0;
+    
+    for (int i = 0; i < 1000000; i++) {
+    	coin = (int)(Math.random()*2);
+    	if(coin == 1) {
+    		heads++;
+    	}else {
+    		tails++;
+    	}
+    }
+    System.out.printf("Heads: %d\nTails: %d",heads, tails);
   }
 }

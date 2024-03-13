1e24be700ba4e066ceb2378917bb4f554bb5c68b
Jonathan Birkey <Jonathan.Birkey@gmail.com>
2024-03-13 11:34:30 -0400

    finished exercise 43

--- src/com/github/jonathanbirkey/chapter05/Exercise43.java
+++ src/com/github/jonathanbirkey/chapter05/Exercise43.java
@@ -2,14 +2,29 @@
  * @author : Jonathan Birkey
  * @mailto : jonathan.birkey@gmail.com
  * @created : 28Feb2024
- *     <p>(Display leap years) Write a program that displays all the leap years, 10 per line, from
- *     101 to 2100, separated by exactly one space. Also display the number of leap years in this
- *     period.
+ *     <p>(Math: combinations) Write a program that displays all possible combinations for picking
+ *     two numbers from integers 1 to 7. Also display the total number of all combinations.
+ *     <p>1 2
+ *     <p>1 3
+ *     <p>...
+ *     <p>...
+ *     <p>The total number of all combinations is 21
  */
 package com.github.jonathanbirkey.chapter05;
 
 public class Exercise43 {
   public static void main(String[] args) {
-    // TODO: solve
+    int totalCombinations = 0;
+
+    for (int i = 1; i <= 7; i++) {
+      for (int j = 1; j <= 7; j++) {
+        if (j != i && j > i) {
+          System.out.printf("%d %d\n", i, j);
+          totalCombinations++;
+        }
+      }
+    }
+
+    System.out.printf("The total number of all combinations is %d", totalCombinations);
   }
 }
